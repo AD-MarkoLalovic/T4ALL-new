1b85c7f98f4f36b0650a6c97f6c1b66db89af648
Marko Lalovic <marko.lalovic@oriontelokom.rs>
2024-12-11 11:23:50 +0100

    update

--- app/src/main/java/com/mobility/enp/view/adapters/PaymentAndPassageAdapter.kt
+++ app/src/main/java/com/mobility/enp/view/adapters/PaymentAndPassageAdapter.kt
@@ -29,76 +29,80 @@ class PaymentAndPassageAdapter(
             binding.paymentAndPassage = card
             binding.viewModel = viewModel
 
+            // Postavljanje za neaktivne i aktivne kartice
             if (card.active == 0) {
-                binding.inactiveCard.visibility = View.VISIBLE
-                binding.itemCard.backgroundTintList = AppCompatResources.getColorStateList(
-                    binding.itemCard.context,
-                    R.color.very_light_gray
-                )
-                binding.horizontalLinePayment.visibility = View.INVISIBLE
-                binding.primaryCard.visibility = View.GONE
-                binding.removeCard.visibility = View.GONE
-                binding.cardFlag.visibility = View.GONE
+                setCardInactive()
             } else {
-                binding.inactiveCard.visibility = View.INVISIBLE
-                binding.itemCard.backgroundTintList = AppCompatResources.getColorStateList(
-                    binding.itemCard.context,
-                    R.color.primary_light_lightest
-                )
-                binding.horizontalLinePayment.visibility = View.VISIBLE
-                binding.primaryCard.visibility = View.VISIBLE
-                binding.removeCard.visibility = View.VISIBLE
-                binding.cardFlag.visibility = View.VISIBLE
+                setCardActive()
+            }
+
+            // Logika za primarnu karticu
+            if (card.defaultCard == 1) {
+                setPrimaryCardStyle()
+            } else {
+                setNonPrimaryCardStyle(card)
             }
 
             binding.executePendingBindings()
+        }
 
-            if (card.defaultCard == 1) {
-                binding.primaryCard.text = binding.root.context.getString(R.string.primary_card)
-                binding.primaryCard.setTextColor(
-                    ContextCompat.getColor(
-                        binding.root.context,
-                        R.color.primary_light_dark
-                    )
-                )
-                binding.removeCard.visibility = View.GONE
-                binding.primaryCard.isClickable = false
+        private fun setCardInactive() {
+            binding.inactiveCard.visibility = View.VISIBLE
+            binding.itemCard.backgroundTintList = AppCompatResources.getColorStateList(
+                binding.itemCard.context,
+                R.color.very_light_gray
+            )
+            binding.horizontalLinePayment.visibility = View.INVISIBLE
+            binding.primaryCard.visibility = View.GONE
+            binding.removeCard.visibility = View.GONE
+            binding.cardFlag.visibility = View.GONE
+        }
 
-                binding.executePendingBindings()
-            } else {
-                val text = binding.root.context.getString(R.string.choose_primary_card)
-                val spannableString = SpannableString(text)
+        private fun setCardActive() {
+            binding.inactiveCard.visibility = View.INVISIBLE
+            binding.itemCard.backgroundTintList = AppCompatResources.getColorStateList(
+                binding.itemCard.context,
+                R.color.primary_light_lightest
+            )
+            binding.horizontalLinePayment.visibility = View.VISIBLE
+            binding.primaryCard.visibility = View.VISIBLE
+            binding.removeCard.visibility = View.VISIBLE
+            binding.cardFlag.visibility = View.VISIBLE
+        }
 
-                spannableString.setSpan(UnderlineSpan(), 0, text.length, 0)
+        private fun setPrimaryCardStyle() {
+            binding.primaryCard.text = binding.root.context.getString(R.string.primary_card)
+            binding.primaryCard.setTextColor(
+                ContextCompat.getColor(binding.root.context, R.color.primary_light_dark)
+            )
+            binding.removeCard.visibility = View.GONE
+            binding.primaryCard.isClickable = false
+        }
 
-                val setTextColor = ContextCompat.getColor(
-                    binding.root.context,
-                    R.color.figmaSplashScreenColor
-                )
-                spannableString.setSpan(ForegroundColorSpan(setTextColor), 0, text.length, 0)
-
-                binding.primaryCard.text = spannableString
-
-                if (card.active == 0) {
-                    binding.removeCard.visibility = View.GONE
-                } else {
-                    binding.removeCard.visibility = View.VISIBLE
-                }
-
-                binding.removeCard.setOnClickListener {
-                    card.id?.let { id -> listener.clickRemoveCard(id.toString()) }
-                }
-
-                binding.primaryCard.isClickable = true
-
-                binding.primaryCard.setOnClickListener {
-                    Log.d("PrimaryCard", "card: ${card.id}")
-                    card.id?.let { id -> listener.setPrimaryCard(id) }
-                }
-                binding.executePendingBindings()
+        private fun setNonPrimaryCardStyle(card: Card) {
+            val context = binding.root.context
+            val text = context.getString(R.string.choose_primary_card)
+            val spannableString = SpannableString(text).apply {
+                setSpan(UnderlineSpan(), 0, text.length, 0)
+                setSpan(ForegroundColorSpan(ContextCompat.getColor(context, R.color.figmaSplashScreenColor)), 0, text.length, 0)
             }
 
+            binding.primaryCard.text = spannableString
+            binding.primaryCard.isClickable = true
+            binding.primaryCard.setOnClickListener {
+                card.id?.let { id -> listener?.setPrimaryCard(id) }
+            }
+
+            if (card.active == 0) {
+                binding.removeCard.visibility = View.GONE
+            } else {
+                binding.removeCard.visibility = View.VISIBLE
+                binding.removeCard.setOnClickListener {
+                    card.id?.let { id -> listener?.clickRemoveCard(id.toString()) }
+                }
+            }
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaymentAndPassageViewHolder {
