367c7bb468728fd9aaf3456ce295fbd30732d7fb
Marko-Orion <marko.dunovic@oriontelekom.rs>
2024-12-17 10:38:46 +0100

    update

--- app/src/main/java/com/mobility/enp/util/NetworkError.kt
+++ app/src/main/java/com/mobility/enp/util/NetworkError.kt
@@ -3,8 +3,8 @@ package com.mobility.enp.util
 import com.mobility.enp.data.model.ApiErrorResponse
 
 sealed class NetworkError : Throwable() {
-    data object NoConnection : NetworkError()
-    data object ServerError : NetworkError()
+    object NoConnection : NetworkError()
+    object ServerError : NetworkError()
     data class ApiError(val errorResponse: ApiErrorResponse) : NetworkError()
 
 }
