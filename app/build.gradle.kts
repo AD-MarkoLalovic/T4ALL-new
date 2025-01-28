ab38c6df16901bd99ff3d3d606de082d25652493
Marko Lalovic <marko.lalovic@oriontelokom.rs>
2025-01-28 14:40:16 +0100

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.mobility.enp"
         minSdk = 29
         targetSdk = 34
-        versionCode = 116
-        versionName = "116"
+        versionCode = 117
+        versionName = "117"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
 
