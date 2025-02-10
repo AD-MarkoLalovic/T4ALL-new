cc346dd21d3d2004c5d7843303c7cb41bba1c70d
Marko-Orion <marko.dunovic@oriontelekom.rs>
2025-02-10 12:34:02 +0100

    incremented version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.mobility.enp"
         minSdk = 29
         targetSdk = 34
-        versionCode = 120
-        versionName = "1.2.0"
+        versionCode = 121
+        versionName = "1.2.1"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
 
