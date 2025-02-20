08e8848a8360ac11a63851c5a391d5e97f26e7b3
Marko-Orion <marko.dunovic@oriontelekom.rs>
2025-02-20 10:16:34 +0100

    fix for list concurence issue

--- app/src/main/java/com/mobility/enp/data/room/database/Converters.kt
+++ app/src/main/java/com/mobility/enp/data/room/database/Converters.kt
@@ -1,93 +1,99 @@
 package com.mobility.enp.data.room.database
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import android.util.Base64
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
-import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.Strictness
 import com.google.gson.reflect.TypeToken
 import com.mobility.enp.data.model.api_home_page.homedata.Data
 import com.mobility.enp.data.model.api_my_invoices.DataMonthly
 import com.mobility.enp.data.model.api_tool_history.listing.InvoiceData
-import java.io.ByteArrayOutputStream
 
 @TypeConverters
 class Converters {
 
-    private val gson = Gson()
-
-    @TypeConverter
-    fun fromDrawable(drawable: Drawable?): String? {
-        if (drawable == null) return null
-        val bitmap = (drawable as BitmapDrawable).bitmap
-        val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-        return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT)
-    }
-
-    @TypeConverter
-    fun toDrawable(base64String: String?): Drawable? {
-        if (base64String == null) return null
-        val bytes = Base64.decode(base64String, Base64.DEFAULT)
-        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-        return BitmapDrawable(null, bitmap)
-    }
+    private val gson = GsonBuilder().setStrictness(Strictness.LENIENT)
+        .serializeNulls()
+        .setPrettyPrinting()
+        .disableHtmlEscaping()
+        .create()
 
     @TypeConverter
     fun fromData(data: Data?): String? {
-        return gson.toJson(data)
+        synchronized(this) {  // Ensures that no other thread modifies data while Gson is serializing it.
+            val safeData = data?.copy() // Ensure a safe copy before serialization
+            return gson.toJson(safeData)
+        }
     }
 
     @TypeConverter
     fun toData(jsonString: String?): Data? {
-        return gson.fromJson(jsonString, Data::class.java)
+        synchronized(this) {
+            return gson.fromJson(jsonString, Data::class.java)
+        }
     }
 
     @TypeConverter
     fun fromDataHistory(data: com.mobility.enp.data.model.api_tool_history.index.Data?): String? {
-        return gson.toJson(data)
+        synchronized(this) {
+            val safeData = data?.copy()
+            return gson.toJson(safeData)
+        }
     }
 
     @TypeConverter
     fun toDataHistory(jsonString: String?): com.mobility.enp.data.model.api_tool_history.index.Data? {
-        return gson.fromJson(
-            jsonString,
-            com.mobility.enp.data.model.api_tool_history.index.Data::class.java
-        )
+        synchronized(this) {
+            return gson.fromJson(
+                jsonString,
+                com.mobility.enp.data.model.api_tool_history.index.Data::class.java
+            )
+        }
     }
 
     @TypeConverter
-    fun fromDataHistoryListing(data: InvoiceData): String? {
-        return gson.toJson(data)
+    fun fromDataHistoryListing(data: InvoiceData?): String? {
+        synchronized(this) {
+            val safeData = data?.copy()
+            return gson.toJson(safeData)
+        }
     }
 
     @TypeConverter
     fun toDataHistoryListing(jsonString: String?): InvoiceData? {
-        return gson.fromJson(jsonString, InvoiceData::class.java)
+        synchronized(this) {
+            return gson.fromJson(jsonString, InvoiceData::class.java)
+        }
     }
 
     @TypeConverter
-    fun fromDataMonthlyBills(data: DataMonthly): String? {
-        return gson.toJson(data)
+    fun fromDataMonthlyBills(data: DataMonthly?): String? {
+        synchronized(this) {
+            val safeData = data?.copy()
+            return gson.toJson(safeData)
+        }
     }
 
     @TypeConverter
     fun toDataMonthlyBills(jsonString: String?): DataMonthly? {
-        return gson.fromJson(jsonString, DataMonthly::class.java)
+        synchronized(this) {
+            return gson.fromJson(jsonString, DataMonthly::class.java)
+        }
     }
 
     @TypeConverter
     fun fromListInt(list: List<Int>?): String? {
-        return gson.toJson(list)
+        synchronized(this) {
+            return gson.toJson(list)
+        }
     }
 
     @TypeConverter
     fun toListInt(jsonString: String?): List<Int> {
-        val type = object : TypeToken<List<Int>>() {}.type
-        return gson.fromJson(jsonString, type)
+        synchronized(this) {
+            val type = object : TypeToken<List<Int>>() {}.type
+            return gson.fromJson(jsonString, type)
+        }
     }
 
 }
