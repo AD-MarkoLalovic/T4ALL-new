0e2fcad785b375a4cee1500319f7dea43d077cac
Marko Lalovic <marko.lalovic@oriontelokom.rs>
2025-02-27 12:56:29 +0100

    updated TollHistoryHomeEntity

--- app/src/main/java/com/mobility/enp/data/room/database/DRoom.kt
+++ app/src/main/java/com/mobility/enp/data/room/database/DRoom.kt
@@ -55,7 +55,7 @@ import kotlinx.coroutines.launch
     entities = [UserLoginResponseRoomTable::class, FcmToken::class, UserLanguage::class, NotificationModel::class, IndexData::class, ToolHistoryListing::class,
         IntroPageStatus::class, ProfileImage::class, MyInvoicesResponse::class, PdfTable::class, LastUser::class, BanksEntity::class, DataRefundRequestEntity::class, CsvTable::class, TagsRefundRequestEntity::class,
         BasicInfoEntity::class, HomeEntity::class, TollHistoryHomeEntity::class, InvoiceHomeEntity::class, InvoiceHomeTotalCurrencyEntity::class, HomeCardsEntity::class, AddedCardsEntity::class],
-    version = 193,
+    version = 195,
     exportSchema = false
 )  // changes on tables require  version of database to be incremented  // also requires database data destruction or migration
 @TypeConverters(Converters::class)
