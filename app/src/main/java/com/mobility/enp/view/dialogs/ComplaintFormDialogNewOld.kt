2f4a75c9252bbce62aca71b9c1c28d7d0cd7f680
Marko Lalovic <marko.lalovic@oriontelokom.rs>
2025-03-06 15:04:18 +0100

    update

--- app/src/main/java/com/mobility/enp/view/dialogs/ComplaintFormDialogNewOld.kt
+++ app/src/main/java/com/mobility/enp/view/dialogs/ComplaintFormDialogNewOld.kt
@@ -1,7 +1,6 @@
 package com.mobility.enp.view.dialogs
 
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
