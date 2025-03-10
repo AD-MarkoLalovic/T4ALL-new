1bdff95880d71a75de2bac5aec67de5075f9e27c
Marko-Orion <marko.dunovic@oriontelekom.rs>
2025-03-10 11:20:44 +0100

    tool history filter fragment

--- app/src/main/java/com/mobility/enp/view/fragments/tool_history/ToolHistoryFilterFragment.kt
+++ app/src/main/java/com/mobility/enp/view/fragments/tool_history/ToolHistoryFilterFragment.kt
@@ -94,7 +94,6 @@ class ToolHistoryFilterFragment : Fragment(), ToolHistoryTagsAdapter.TagSend {
         binding.chkBox.setOnClickListener {
             val isChecked = binding.chkBox.isChecked
             vModel.allTagsSelected = isChecked
-            setCheckboxColors(isChecked)
         }
 
         vModel.startDate.observe(viewLifecycleOwner) { data ->
@@ -150,10 +149,38 @@ class ToolHistoryFilterFragment : Fragment(), ToolHistoryTagsAdapter.TagSend {
     }
 
     private fun setFranchiser() {
-        franchiseViewModel.franchiseModel.observe(viewLifecycleOwner){franchiseModel ->
-            franchiseModel?.franchisePrimaryColor?.let {
-                binding.btnSearch.backgroundTintList = ColorStateList.valueOf(it)
-                binding.exportBlock.setTextColor(it)
+        franchiseViewModel.franchiseModel.observe(viewLifecycleOwner) { franchiseModel ->
+            franchiseModel?.franchisePrimaryColor?.let { color ->
+                binding.btnSearch.backgroundTintList = ColorStateList.valueOf(color)
+                binding.exportBlock.setTextColor(color)
+
+                binding.searchMark.setImageResource(franchiseModel.loopIcon)
+
+                val states = arrayOf(
+                    intArrayOf(android.R.attr.state_checked),  // When switch is ON
+                    intArrayOf(-android.R.attr.state_checked) // When switch is OFF
+                )
+
+                val colors = intArrayOf(
+                    color,  // ON color
+                    ContextCompat.getColor(requireContext(), R.color.primary_light_dark) // OFF color
+                )
+
+                val colorStateList = ColorStateList(states, colors)
+                binding.chkBox.buttonTintList = colorStateList
+            }?:run {
+                val states = arrayOf(
+                    intArrayOf(android.R.attr.state_checked),  // When switch is ON
+                    intArrayOf(-android.R.attr.state_checked) // When switch is OFF
+                )
+
+                val colors = intArrayOf(
+                    ContextCompat.getColor(requireContext(), R.color.figmaSplashScreenColor),  // ON color
+                    ContextCompat.getColor(requireContext(), R.color.primary_light_dark) // OFF color
+                )
+
+                val colorStateList = ColorStateList(states, colors)
+                binding.chkBox.buttonTintList = colorStateList
             }
         }
     }
@@ -272,7 +299,7 @@ class ToolHistoryFilterFragment : Fragment(), ToolHistoryTagsAdapter.TagSend {
                 vModel.tagSerials = index.tags as ArrayList<Tag>
                 Log.d(TAG, "setObservers: ${vModel.tagSerials}")
 
-                val adapter = ToolHistoryTagsAdapter(vModel.tagSerials, this)
+                val adapter = ToolHistoryTagsAdapter(vModel.tagSerials, this,franchiseViewModel)
 
                 binding.cycler.adapter = adapter
                 binding.cycler.layoutManager = LinearLayoutManager(context)
@@ -307,24 +334,6 @@ class ToolHistoryFilterFragment : Fragment(), ToolHistoryTagsAdapter.TagSend {
     override fun onTagRemove(tag: Tag) {
         vModel.selectedTags.remove(tag)
         Log.d(TAG, "onSendTag: ${vModel.selectedTags}")
-    }
-
-    private fun setCheckboxColors(isChecked: Boolean) {
-        if (isChecked) {
-            binding.chkBox.buttonTintList = ColorStateList.valueOf(
-                ContextCompat.getColor(
-                    requireContext(),
-                    R.color.figmaSplashScreenColor
-                )
-            )
-        } else {
-            binding.chkBox.buttonTintList = ColorStateList.valueOf(
-                ContextCompat.getColor(
-                    requireContext(),
-                    R.color.primary_light_dark
-                )
-            )
-        }
     }
 
     override fun onDestroyView() {
