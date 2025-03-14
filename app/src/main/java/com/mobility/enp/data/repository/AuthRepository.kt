51c5cc4a3ef9cd23fddabf12fa1e2addbceee19a
Marko Lalovic <marko.lalovic@oriontelokom.rs>
2025-03-14 13:45:13 +0100

    refactoring registration flow

--- app/src/main/java/com/mobility/enp/data/repository/AuthRepository.kt
+++ app/src/main/java/com/mobility/enp/data/repository/AuthRepository.kt
@@ -1,13 +1,15 @@
 package com.mobility.enp.data.repository
 
 import android.content.Context
+import com.mobility.enp.R
 import com.mobility.enp.data.model.api_room_models.UserLanguage
+import com.mobility.enp.data.model.registration.RegistrationCountry
 import com.mobility.enp.data.room.database.DRoom
 
 /**
  * Odgovornost: Upravljanje autentifikacijom i funkcionalnostima za korisnički nalog.
  * Logovanje korisnika, Promena lozinke,Resetovanje lozinke,Deaktivacija korisničkog naloga.
- * Promena jezika
+ * Promena jezika> Registracija korisnika
  */
 
 class AuthRepository(database: DRoom, context: Context) : BaseRepository(database, context) {
@@ -25,5 +27,28 @@ class AuthRepository(database: DRoom, context: Context) : BaseRepository(databas
 
     suspend fun saveLanguage(language: UserLanguage) {
         database.languageDao().insertLanguage(language)
+    }
+
+    /**
+     * Registration
+     */
+    fun getCountries(): List<RegistrationCountry> {
+        return listOf(
+            RegistrationCountry(
+                context.getString(R.string.serbia_country_code),
+                context.getString(R.string.serbia),
+                R.drawable.serbia_flag
+            ),
+            RegistrationCountry(
+                context.getString(R.string.macedonia_country_code),
+                context.getString(R.string.macedonia),
+                R.drawable.macedonia_flag
+            ),
+            RegistrationCountry(
+                context.getString(R.string.montenegro_country_code),
+                context.getString(R.string.montenegro),
+                R.drawable.montenegro_flag
+            )
+        )
     }
 }
--- app/src/main/java/com/mobility/enp/view/fragments/LocationSelectFragment.kt
+++ app/src/main/java/com/mobility/enp/view/fragments/LocationSelectFragment.kt
@@ -1,31 +1,27 @@
 package com.mobility.enp.view.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.mobility.enp.Config
 import com.mobility.enp.R
-import com.mobility.enp.data.model.registration.CountryModel
 import com.mobility.enp.databinding.FragmentLocationSelectBinding
 import com.mobility.enp.view.adapters.SelectCountryAdapter
-import com.mobility.enp.viewmodel.LoginViewModel
+import com.mobility.enp.viewmodel.LocationSelectViewModel
 
 class LocationSelectFragment : Fragment() {
 
     private var _binding: FragmentLocationSelectBinding? = null
     private val binding: FragmentLocationSelectBinding get() = _binding!!
     private lateinit var adapter: SelectCountryAdapter
-    private lateinit var adapterData: ArrayList<CountryModel>
-    private lateinit var selectedCountry: CountryModel
-    private val viewModel: LoginViewModel by activityViewModels()
+
+    private val viewModel: LocationSelectViewModel by viewModels { LocationSelectViewModel.Factory }
+    private var selectedCountry: String? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,50 +35,21 @@ class LocationSelectFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        adapterData = arrayListOf<CountryModel>()
-        context?.let {
-            viewModel.initDatabase()
-            adapterData.add(
-                CountryModel(
-                    getString(R.string.serbia_country_code),
-                    getString(R.string.serbia),
-                    AppCompatResources.getDrawable(it, R.drawable.serbia_flag)
-                )
-            )
-            adapterData.add(
-                CountryModel(
-                    getString(R.string.macedonia_country_code),
-                    getString(R.string.macedonia),
-                    AppCompatResources.getDrawable(it, R.drawable.macedonia_flag)
-                )
-            )
-            adapterData.add(
-                CountryModel(
-                    getString(R.string.montenegro_country_code),
-                    getString(R.string.montenegro),
-                    AppCompatResources.getDrawable(it, R.drawable.montenegro_flag)
-                )
-            )
 
-            adapter = SelectCountryAdapter(adapterData)
-            adapter.setInter(object : SelectCountryAdapter.SelectedCountry {
-                override fun pickedCountry(county: CountryModel) {
-                    selectedCountry = county
-                    Log.d(Config.TAGREG, "selected : $selectedCountry")
-                }
-            })
-            binding.cycler.adapter = adapter
-            binding.cycler.layoutManager = LinearLayoutManager(context)
-            selectedCountry = adapterData[0] // initially set to 0
-            Log.d(Config.TAGREG, "selected : $selectedCountry")
+        val countries = viewModel.fetchCountries()
+        adapter = SelectCountryAdapter(countries) { country ->
+            selectedCountry = country
         }
+        binding.rvListCountries.adapter = adapter
 
         binding.button.setOnClickListener {
-            findNavController().navigate(
-                LocationSelectFragmentDirections.actionLocationSelectFragmentToTosFragment(
-                    selectedCountry
+            selectedCountry?.let { countryCode ->
+                findNavController().navigate(
+                    LocationSelectFragmentDirections.actionLocationSelectFragmentToTosFragment(
+                        countryCode
+                    )
                 )
-            )
+            }
         }
 
         binding.backArrow.setOnClickListener {
