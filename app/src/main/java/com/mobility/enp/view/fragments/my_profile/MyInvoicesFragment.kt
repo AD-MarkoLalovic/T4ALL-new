8bf1842b4e4ebe3e76f8f57982d2d10d1360cd1e
Marko Lalovic <marko.lalovic@oriontelokom.rs>
2025-03-25 10:13:45 +0100

    up versionCode

--- app/src/main/java/com/mobility/enp/view/fragments/my_profile/MyInvoicesFragment.kt
+++ app/src/main/java/com/mobility/enp/view/fragments/my_profile/MyInvoicesFragment.kt
@@ -1,7 +1,6 @@
 package com.mobility.enp.view.fragments.my_profile
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
