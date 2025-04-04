27b6eb3e8e0c02636dbc3e609d98704f29209b1c
Marko Lalovic <marko.lalovic@oriontelokom.rs>
2025-04-04 14:58:37 +0200

    update

--- app/src/main/java/com/mobility/enp/view/fragments/my_profile/ChangePasswordFragment.kt
+++ app/src/main/java/com/mobility/enp/view/fragments/my_profile/ChangePasswordFragment.kt
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
