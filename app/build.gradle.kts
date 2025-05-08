6829a6e16aa5530037e9c1d1b674a417f1c29bed
Marko <phoenex1990@gmail.com>
2025-05-08 03:59:57 +0200

    incremented version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.mobility.enp"
         minSdk = 29
         targetSdk = 34
-        versionCode = 155
-        versionName = "1.3.5"
+        versionCode = 157
+        versionName = "1.3.6"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
 
