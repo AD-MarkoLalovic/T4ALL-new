6a3873bd5485cea87cc15670d8a924b0a36d424c
Marko <phoenex1990@gmail.com>
2025-08-04 09:59:16 +0200

    pdf api rework

--- app/src/main/java/com/mobility/enp/data/repository/BillsRepository.kt
+++ app/src/main/java/com/mobility/enp/data/repository/BillsRepository.kt
@@ -2,12 +2,16 @@ package com.mobility.enp.data.repository
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.mobility.enp.data.model.ErrorBody
+import com.mobility.enp.data.model.api_my_invoices.BillDownload
 import com.mobility.enp.data.model.api_my_invoices.BillsDetailsResponse
 import com.mobility.enp.data.model.api_my_invoices.refactor.MyInvoicesResponse
 import com.mobility.enp.data.model.pdf_table.PdfTable
 import com.mobility.enp.data.repository.PassageHistoryRepository.Companion.TAG
 import com.mobility.enp.data.room.database.DRoom
 import com.mobility.enp.util.NetworkError
+import com.mobility.enp.view.adapters.my_invoices_adapters.BillsDetailsAdapter
 
 class BillsRepository(dRoom: DRoom, context: Context) : BaseRepository(dRoom, context) {
 
@@ -55,6 +59,39 @@ class BillsRepository(dRoom: DRoom, context: Context) : BaseRepository(dRoom, co
             return try {
                 val response =
                     apiService(token).getInvoicesPerMonth(getLangKey(), 1, perPage, selectedCountry)
+                if (response.isSuccessful) {
+                    response.body()?.let { indexData ->
+                        Result.success(indexData)
+                    } ?: Result.failure(NetworkError.ServerError)
+                } else {
+                    response.errorBody()?.let { errorBody ->
+                        val errorResponse = parseErrorResponse(response.code(), errorBody)
+                        Result.failure(NetworkError.ApiError(errorResponse))
+                    } ?: Result.failure(NetworkError.ServerError)
+                }
+            } catch (e: Exception) {
+                Log.d(TAG, "getIndexData: ${e.message} ${e.cause}")
+                Result.failure(NetworkError.ServerError)
+            }
+        }
+
+        return Result.failure(NetworkError.ServerError)
+    }
+
+    suspend fun downloadPdfData(
+        billId: String,
+    ): Result<BillDownload> {
+
+        if (!isNetworkAvailable()) {
+            return Result.failure(NetworkError.NoConnection)
+        }
+
+        val userToken = getUserToken()
+
+        userToken?.let { token ->
+            return try {
+                val response =
+                    apiService(token).getPdfBill(billId)
                 if (response.isSuccessful) {
                     response.body()?.let { indexData ->
                         Result.success(indexData)
