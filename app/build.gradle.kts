1d3c36643b5c426a1fbb9798dd53cf3e3f0ee33e
Marko Dunovic <marko.dunovic@oriontelekom.rs>
2025-12-03 17:36:21 +0100

    optimized filter for filter fragment of toll history to be in line with other parts of the application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.mobility.enp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 224
-        versionName = "1.7.0"
+        versionCode = 225
+        versionName = "1.7.1"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
 
