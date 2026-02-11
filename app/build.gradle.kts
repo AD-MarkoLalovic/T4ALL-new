8b73e2c3f6d3fe7db92f315e14e3148297b2d8a3
Marko Dunovic <marko.dunovic@oriontelekom.rs>
2026-02-11 09:21:26 +0100

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.mobility.enp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 232
-        versionName = "1.7.4"
+        versionCode = 233
+        versionName = "1.7.5"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
 
