0b4d87cf9770b75fc4a3e31b4966b73c22b1f900
marko.lalovic <marko.lalovic@oriontelekom.rs>
2026-03-12 15:15:47 +0100

    chore: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.mobility.enp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 247
+        versionCode = 248
         versionName = "1.8.2"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
