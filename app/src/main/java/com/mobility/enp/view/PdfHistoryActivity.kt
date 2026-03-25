26032b951e6e0b4c2c686f576864500269ea9983
marko-dunovic <marko.dunovic@oriontelekom.rs>
2026-03-25 13:22:37 +0100

    update

--- app/src/main/java/com/mobility/enp/view/PdfHistoryActivity.kt
+++ app/src/main/java/com/mobility/enp/view/PdfHistoryActivity.kt
@@ -1,19 +1,17 @@
 package com.mobility.enp.view
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
-import com.mobility.enp.R
-import com.mobility.enp.databinding.ActivityCsvBinding
 import com.mobility.enp.databinding.ActivityPdfHistoryBinding
 import com.mobility.enp.viewmodel.UserPassViewModel
 import kotlinx.coroutines.launch
-import kotlin.getValue
 
 class PdfHistoryActivity : AppCompatActivity() {
     private val vModel: UserPassViewModel by viewModels { UserPassViewModel.Factory }
@@ -38,10 +36,28 @@ class PdfHistoryActivity : AppCompatActivity() {
             val byteArray = vModel.fetchPDFData()
             byteArray?.let {
                 binding.tableView.fromBytes(it).load()
+                savePdfToDownloads(byteArray, "history_export_pdf_${System.currentTimeMillis()}")
             } ?: run {
                 Log.d(TAG, "loadCSV: no data")
             }
+        }
+    }
 
+    fun savePdfToDownloads(bytes: ByteArray, fileName: String) {
+        val resolver = contentResolver
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "$fileName.pdf")
+            put(MediaStore.MediaColumns.MIME_TYPE, "application/pdf")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+        }
+
+        val uri = resolver.insert(MediaStore.Files.getContentUri("external"), contentValues)
+
+        uri?.let {
+            resolver.openOutputStream(it)?.use { output ->
+                output.write(bytes)
+            }
         }
     }
 }
