70ac9b984d0186887870f4f9a3d6b7e937f24b8c
marko-dunovic <marko.dunovic@oriontelekom.rs>
2026-04-29 10:45:16 +0200

    incremented gradle and room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.mobility.enp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 252
+        versionCode = 253
         versionName = "1.10.3"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
