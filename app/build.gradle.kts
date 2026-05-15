c935f2c7a062640bb4b14c455ce2c0d86a526e86
marko.dunovic <marko.dunovic@oriontelekom.rs>
2026-05-15 13:41:17 +0200

    incremented gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 258
-        versionName = "1.10.6"
+        versionName = "1.11.0"
 
         resourceConfigurations += listOf("en", "sr", "de", "mk", "tr", "b+cnr", "hr", "el", "bs")
 
