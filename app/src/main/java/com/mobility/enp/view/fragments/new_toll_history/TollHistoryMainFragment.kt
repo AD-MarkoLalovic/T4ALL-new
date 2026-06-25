7b89692d40434171335069d6e9222030cb8b69bd
marko.lalovic <marko.lalovic@oriontelekom.rs>
2026-06-25 08:56:23 +0200

    refactor(toll-history): rename main fragment and view model

--- app/src/main/java/com/mobility/enp/view/fragments/new_toll_history/TollHistoryMainFragment.kt
+++ app/src/main/java/com/mobility/enp/view/fragments/new_toll_history/TollHistoryMainFragment.kt
@@ -35,15 +35,15 @@ import com.mobility.enp.view.dialogs.ChangePasswordDialog
 import com.mobility.enp.view.dialogs.GeneralMessageDialog
 import com.mobility.enp.view.dialogs.GeneralMessageDialogInfoButton
 import com.mobility.enp.viewmodel.FranchiseViewModel
-import com.mobility.enp.viewmodel.toll_history.NewTollHistoryViewModel
+import com.mobility.enp.viewmodel.toll_history.TollHistoryMainViewModel
 import kotlinx.coroutines.launch
 
-class TollHistoryMain : Fragment() {
+class TollHistoryMainFragment : Fragment() {
 
     private var _binding: FragmentTollHistoryMainBinding? = null
     private val binding: FragmentTollHistoryMainBinding get() = _binding!!
 
-    private val viewModel: NewTollHistoryViewModel by viewModels { NewTollHistoryViewModel.factory }
+    private val viewModel: TollHistoryMainViewModel by viewModels { TollHistoryMainViewModel.factory }
     private val franchiseViewModel: FranchiseViewModel by activityViewModels { FranchiseViewModel.Factory }
 
     private lateinit var pagingAdapter: TollHistoryPagingAdapter
