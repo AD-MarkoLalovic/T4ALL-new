9b0539c08e227a0ebaf70d4acd9b9d54e31b774b
Marko Dunovic <phoenex1990@gmail.com>
2026-07-07 08:23:19 +0200

    update: updated project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,5 @@ buildscript {
 }
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.gms.google.services) apply false
 }
